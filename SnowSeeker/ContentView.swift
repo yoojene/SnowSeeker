29f4823f0242ac90d3c87731186382eb1717ed54
Eugene Cross <eugene.cross@gmail.com>
2023-10-10 17:28:39 +0100

    Day 97 - SnowSeeker, adding searchabilitty

--- SnowSeeker/ContentView.swift
+++ SnowSeeker/ContentView.swift
@@ -22,10 +22,15 @@ extension View {
 struct ContentView: View {
     
     let resorts: [Resort] = Bundle.main.decode("resorts.json")
+    
+    @State private var searchText = ""
+    
+    
+
 
     var body: some View {
         NavigationView {
-            List(resorts) { resort in
+            List(filteredResorts) { resort in
                 NavigationLink {
                     ResortView(resort: resort)
                 } label: {
@@ -50,10 +55,19 @@ struct ContentView: View {
                 }
             }
             .navigationTitle("Resorts")
+            .searchable(text: $searchText, prompt: "Search for a resort")
             
             WelcomeView()
         }
         .phoneOnlyNavigationView()
+    }
+    
+    var filteredResorts: [Resort] {
+        if searchText.isEmpty {
+            return resorts
+        } else {
+            return resorts.filter { $0.name.localizedCaseInsensitiveContains(searchText) }
+        }
     }
 
 }
