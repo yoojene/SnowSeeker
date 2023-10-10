dbcf899cfb7877765bf8e8887a8ab9f9696dc123
Eugene Cross <eugene.cross@gmail.com>
2023-10-10 15:37:52 +0100

    Initial Commit

---
+++ SnowSeeker/SnowSeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowSeekerApp.swift
+//  SnowSeeker
+//
+//  Created by Eugene on 10/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct SnowSeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
