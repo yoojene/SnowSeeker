ddd7e9c9df4722f148efa86e072c4bec666b320f
Eugene Cross <eugene.cross@gmail.com>
2023-10-10 16:41:55 +0100

    Day 97 - SnowSeeker, building primary list of itens

---
+++ SnowSeeker/Resorts.swift
@@ -0,0 +1,25 @@
+//
+//  Resorts.swift
+//  SnowSeeker
+//
+//  Created by Eugene on 10/10/2023.
+//
+
+import Foundation
+
+struct Resort: Codable, Identifiable {
+    let id: String
+    let name: String
+    let country: String
+    let description: String
+    let imageCredit: String
+    let price: Int
+    let size: Int
+    let snowDepth: Int
+    let elevation: Int
+    let runs: Int
+    let facilities: [String]
+    
+    static let allResorts: [Resort] = Bundle.main.decode("resorts.json")
+    static let example = allResorts[0]
+}
